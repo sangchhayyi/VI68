65587140fadd8020cd9557b475c482828b290264
sangchhayyi <sangchhayyi@gmail.com>
2024-06-28 19:10:14 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const openMenu=function(){
+    let menu=document.getElementsByClassName("navbar")[0];
+    menu.style.left="0%";
+}
+const closeMenu=function(){
+    let menu=document.getElementsByClassName("navbar")[0];
+    menu.style.left="-100%";
+}
